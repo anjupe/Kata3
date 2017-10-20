7b4fdfa242a139586f51e1db14adae618e7c6c41
anjupe <anjupe@gmail.com>
2017-10-20 17:39:48 +0100

    Kata3 Segunda versión con la clase histogram

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -37,10 +37,9 @@ public class HistogramDisplay extends ApplicationFrame{
     }
     private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(15, "", "ulpgc.es");
-        dataSet.addValue(1, "", "hotmail.com");
-        dataSet.addValue(7, "", "ull.es");
-        dataSet.addValue(10, "", "gmail.com");
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
         return dataSet;
     }   
 }
