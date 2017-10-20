815426f8685dbd382983a1883be32ae55c41b170
anjupe <anjupe@gmail.com>
2017-10-20 17:47:51 +0100

    Kata3 Segunda versión con la clase histogram

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -5,18 +5,10 @@ public class Kata3 {
     public static void main(String[] args) {
         
         Histogram histogram = new Histogram();
-        for (int i =0; i < 10; i++){
-            histogram.increment("ulpgc.es");
-        }
-        for (int i =0; i < 5; i++){
-            histogram.increment("hotmail.com");
-        }
-        for (int i =0; i < 2; i++){
-            histogram.increment("ull.es");
-        }
-        for (int i =0; i < 7; i++){
-            histogram.increment("gmail.com");
-        } 
+        histogram.increment("ulpgc.es",10);
+        histogram.increment("hotmail.com",5);
+        histogram.increment("ull.es",2);
+        histogram.increment("gmail.com",7);
         new HistogramDisplay(histogram).execute();
     }
     
